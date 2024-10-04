fdf0648cd309fa1c8278da0ae44bd897867091cb
Anil Kumar K K <akumarkk@outlook.com>
2024-10-03 21:15:52 -0700

    py3 logging

---
+++ Py/prog/log.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(filename="logfile.log", level=logging.DEBUG)
+logging.info("info")
+logging.debug("debug")
+logging.warn("warn")
+logging.error("error")
+logging.critical("critical")

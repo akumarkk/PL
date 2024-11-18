d7bf07cb4ab2af74e27387b51fc41935c5bc631c
Anil Kumar K K <akumarkk@outlook.com>
2024-11-18 15:15:57 -0800

    map

--- JS/prog/map.js
+++ JS/prog/map.js
@@ -9,3 +9,37 @@ t= console.timeEnd();
 e = performance.now()
 
 console.log(m, `"completed in ${e-s} microsecs"`);
+
+
+function sum(num) {
+    let ret = 0;
+    for (var i = 0; i < num; i++) {
+        ret += i;
+    }
+    return ret;
+}
+
+const memoize = (fn) => {
+let cache = {};
+
+    return function(n) {
+        if (cache[n] !== undefined) {
+            return cache[n];
+        }
+
+        cache[n] = fn(n);
+        return cache[n];
+        
+    }
+}
+
+let sumFn =  memoize(sum);
+s = performance.now();
+sumFn(10000);
+e = performance.now();
+console.log(m, `"sumFn completed in ${e-s} microsecs"`);
+
+s = performance.now();
+sumFn(10000);
+e = performance.now();
+console.log(m, `"sumFn completed in ${e-s} microsecs"`);

2eb2195eb59998167616d93f9445cb59e97c645a
Anil Kumar K K <akumarkk@outlook.com>
2024-11-22 16:08:40 -0800

    class var

--- Py/advpy.md
+++ Py/advpy.md
@@ -2,3 +2,15 @@
 
 dataclasses:
     similar to record in high-level prog languages.
+    dataclasses provide a convenient way to define classes with automatically generated methods like __init__,__repr__ etc
+    - The @dataclass decorator generates an __init__ method that looks like this:
+    ```
+    def __init__(self, name: str, age: int, city: str):
+        pass
+    ```
+
+    - Post-Initialization: For additional initialization tasks, you can use the __post_init__ method, which is called after the __init__ method.
+
+
+class var
+when an instance modifies the class variable, it creates its own copy, which leads to a different behavior.
--- Py/prog/dataclassespy.py
+++ Py/prog/dataclassespy.py
@@ -1,12 +1,19 @@
 from dataclasses import dataclass, field
+import random
 import logging
 logger = logging.getLogger(__name__)
 
 @dataclass
 class Product:
+    # static var
+    stat = "cal"
     name:str = field(compare=True)
     category:str = field(compare=True)
     desc:str = field(compare=False)
+
+    # def __init__(self):
+    #     self.pid = random.random()
+
 
 def main():
     logging.basicConfig(level=logging.INFO)
@@ -17,5 +24,29 @@ def main():
     logger.info(f"mango == mexmango {mexmango == mango}")
     logger.info(f"mango == mangmango {mangmango == mango}")
 
+    logger.info(Product.stat)
+    logger.info(mango.stat)
+    mango.stat = 'change'
+    logger.info(Product.stat)
+    logger.info(mango.stat)
+    Product.stat = 'change1'
+    logger.info(Product.stat)
+    logger.info(mango.stat)
 main()
 
+
+class Prod:
+    class_var = 10
+
+p1 = Prod()
+p2= Prod()
+logger.info(f"p1 = {p1.class_var} p2 = {p2.class_var} {Prod.class_var}")
+p1.class_var=20
+p2.class_var=200
+logger.info(f"p1 = {p1.class_var} p2 = {p2.class_var} {Prod.class_var}")
+
+Prod.class_var=100
+logger.info(f"p1 = {p1.class_var} p2 = {p2.class_var} {Prod.class_var}")
+
+Prod.class_var=110
+logger.info(f"p1 = {p1.class_var} p2 = {p2.class_var} {Prod.class_var}")

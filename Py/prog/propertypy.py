44a3deb1dff0b82028d2b649051f42fe9457c403
Anil Kumar K K <akumarkk@outlook.com>
2024-11-25 17:58:32 -0800

    property

---
+++ Py/prog/propertypy.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    @property
+    def area(self):
+        return self.w * self.h
+
+
+
+r = Rectangle(100, 10)
+print(r.area)

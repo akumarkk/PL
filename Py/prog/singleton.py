8d807b424ebf42e37fd3e9543665614b3c41dcdc
Anil Kumar K K <akumarkk@outlook.com>
2024-11-26 21:45:54 -0800

    singleton

---
+++ Py/prog/singleton.py
@@ -0,0 +1,27 @@
+class EleSingleton:
+    _instance = None
+    count = 0
+    
+
+    def __init__(self, val):
+        print(f"v {EleSingleton.count} {val} ")
+        EleSingleton.count = 1 + EleSingleton.count
+        if EleSingleton.count == 1:
+            print("init")
+            self.val = val
+
+    def __new__(cls, *args, **kwargs):
+        print(args, kwargs)
+        if cls._instance is None:
+            print(cls._instance)
+            cls._instance = super().__new__(cls)
+        print(cls._instance)
+        return cls._instance
+
+
+
+e1 = EleSingleton(10)
+e2 = EleSingleton(11)
+e23 = EleSingleton(117)
+print(e1.count, e2.count, e23.count)
+print(e1.val, e2.val, e23.val)

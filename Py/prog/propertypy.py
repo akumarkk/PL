bcfb7c2f25b11688eea91778f201e168dadd2290
Anil Kumar K K <akumarkk@outlook.com>
2024-11-26 22:57:48 -0800

    setter

--- Py/prog/propertypy.py
+++ Py/prog/propertypy.py
@@ -2,12 +2,19 @@ class Rectangle:
     def __init__(self, w, h):
         self.w = w
         self.h = h
+        self._area = w * h
 
     @property
     def area(self):
-        return self.w * self.h
+        return self.w * self.h if self._area is None else self._area
+    
+    @area.setter
+    def area(self, val):
+        self._area = val
 
 
 
 r = Rectangle(100, 10)
+print(r.area)
+r.area = 1010
 print(r.area)

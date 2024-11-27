cd5966adfa54198f5fe38ba33a60b10d090eadbc
Anil Kumar K K <akumarkk@outlook.com>
2024-11-27 12:37:45 -0800

    jy py from jar

--- Py/prog/jy/pyjy.py
+++ Py/prog/jy/pyjy.py
@@ -1,3 +1,9 @@
 from java.lang import System
+from org.json import JSONObject
 
 System.out.println("hello java!")
+
+jo = JSONObject("{ \"abc\" : \"def\" }")
+System.out.println(jo)
+
+# export CLASSPATH=/Users/anikrisms/Downloads/json-20140107.jar

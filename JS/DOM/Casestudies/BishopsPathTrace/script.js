32b7285ec7aee627cfc34a6cd20ab9e87c16a611
Anil Kumar K K <akumarkk@outlook.com>
2024-12-25 18:05:14 -0800

    Bishop path

---
+++ JS/DOM/Casestudies/BishopsPathTrace/script.js
@@ -0,0 +1,63 @@
+let board = document.querySelector('.board');
+let start = false;
+let end = false;
+let path = new Map();
+
+board.addEventListener('click', (e) => {
+    console.log(`${start} ${end}`);
+    if (!start) {
+
+        path.set(e.target, e.target.style.backgroundColor);
+        start = true;
+        e.target.style.backgroundColor = 'red';
+        
+    } else if (start) {
+        // Additionally as a bonus, you can also try to refresh the board if the user clicks on some other checkbox, and then trace the path from the new checkbox. (Code for this is provided in the solution)
+
+        path.forEach((color, element) => {
+            element.style.backgroundColor = color;
+            path.delete(element);
+        });
+        
+        start = true;
+        path.set(e.target, e.target.style.backgroundColor);
+        e.target.style.backgroundColor = 'red';
+        console.log("end clicked");
+        // end.style.backgroundColor = 'yellow';
+    }
+    
+    else if (!end) {
+        if(!path.has(e.target)) {
+            path.set(e.target, e.target.style.backgroundColor);
+        }
+        
+        end = true;
+        e.target.style.backgroundColor = 'red';
+        console.log("end clicked");
+        // end.style.backgroundColor = 'yellow';
+    } else {
+        path.forEach((color, element) => {
+            element.style.backgroundColor = color;
+        });
+        start = false;
+        end = false;
+    }
+
+});
+
+board.addEventListener('mouseover', (e) => {
+
+
+    if (!start) {
+        return;
+
+    }
+
+    if(path.has(e.target)) {
+        return;
+    }
+
+    path.set(e.target, e.target.style.backgroundColor);
+    e.target.style.backgroundColor = 'red';
+
+});

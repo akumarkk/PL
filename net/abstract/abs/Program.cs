8a1525d32c0eacfce86a1d6f8d2e103123d9b0f5
Anil Kumar K K <akumarkk@outlook.com>
2025-02-27 13:27:12 -0800

    virt func table

---
+++ net/abstract/abs/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Program;
+
+class Program : Base
+{
+    public void Print() {
+        Console.WriteLine("Print from Program!");
+    }   
+}
+
+
+class MainClass : Program {
+
+    public void Print() {
+        Console.WriteLine("Print from derived!");
+    }
+
+
+    public static void Main() {
+        new MainClass().Print();
+
+        Program p = new MainClass();
+        p.Print();
+
+        Base b = new MainClass();
+        b.Print();
+    }
+
+}
+
+class Base {
+
+    public virtual void Print() {
+        Console.WriteLine("Print from base!");
+    }   
+}

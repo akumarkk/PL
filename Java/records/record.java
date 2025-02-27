e7911f7cb91f87fb966a93650e75cba45b955ab5
Anil Kumar K K <akumarkk@outlook.com>
2025-02-27 11:45:05 -0800

    record in Java

--- Java/records/record.java
+++ Java/records/record.java
@@ -1,5 +1,31 @@
 // import person.Person;
+
+
+
 class Main {
+private class EqualObj {
+    int val;
+    String name;
+
+    public EqualObj(int val, String name) {
+        this.val = val;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof EqualObj) {
+            EqualObj another = (EqualObj) obj;
+            return val == another.val && name.equals(another.name);
+        }
+        return false;
+    }
+}
+
+public EqualObj getEqualObj() {
+    return new EqualObj(10, "Aniket");
+}
+
     public static void main(String[] args) {
         Person person = new Person("Aniket", 20);
         System.out.println(person);
@@ -12,5 +38,12 @@ class Main {
 
         System.out.println("person1 == person = %b perons.equals(person1) = %b".formatted(person1 == person, person.equals(person1)));
         System.out.println("person1 == person2 = %b, person1.equals(person2) = %b".formatted(person1 == person2, person1.equals(person2)));
+    
+        Main main = new Main();
+        EqualObj equal = main.getEqualObj();
+        EqualObj equal1 = main.getEqualObj();
+        System.out.println(equal == equal1);
+
+        System.out.println(equal.equals(equal1));
     }
 }

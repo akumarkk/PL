d8d3fe3f624953d5ede458861b4ff5e28d824209
Anil Kumar K K <akumarkk@outlook.com>
2025-02-27 20:30:34 -0800

    exception chaining

--- Java/Basics/excep.java
+++ Java/Basics/excep.java
@@ -1,12 +1,18 @@
+import java.lang.Exception;
 public class Program {
 
     public static void main(String[] args) {
         try{
-            int a = 10/0;
-        } catch(Exception e) {
+            throw new IllegalArgumentException("Invalid exception");
+            // int a = 10/0;
+        } catch(ArithmeticException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        catch(Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
             // System.out.println(e.printStackTrace());
-        }
+        } 
     }
 }

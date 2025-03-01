e3ac168a48677b3aa2a621859b171926a628b93a
Anil Kumar K K <akumarkk@outlook.com>
2025-03-01 00:50:53 -0800

    spring app

---
+++ Java/springboot/springboot/src/main/java/com/example/demo/MyprojectApplication.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class MyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectApplication.class, args);
+	}
+
+
+	private List<String> data = new ArrayList<>();
+	@GetMapping("/get")
+    public List<String> getData() {
+        return data;
+    }
+
+    @PostMapping("/post")
+    public String postData(@RequestBody String newData) {
+        data.add(newData);
+        return "Data added: " + newData;
+    }
+
+	@GetMapping("/adddata")
+    public String postSampleData() {
+        data.add("Yash");
+		data.add("Punith");
+		data.add("Suraj");
+        return "Data added: " + data;
+    }
+
+    @GetMapping("/get/{index}")
+    public String getDataByIndex(@PathVariable int index) {
+        if (index >= 0 && index < data.size()) {
+            return data.get(index);
+        } else {
+            return "Index out of bounds";
+        }
+    }
+
+    @DeleteMapping("/delete/{index}")
+    public String deleteDataByIndex(@PathVariable int index){
+        if(index >=0 && index < data.size()){
+            String removed = data.remove(index);
+            return "removed: " + removed;
+        } else {
+            return "index out of bounds";
+        }
+    }
+
+}
